db18790918fabfaae0e56397c0415ed44e5689cc
sdauk555 <samdauk5@gmail.com>
2021-07-16 08:53:31 -0600

    added volume keybindings comment

--- config.h
+++ config.h
@@ -1,5 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
+#include "movestack.c"
+#include <X11/XF86keysym.h>
+
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
@@ -64,12 +67,18 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+
+/* custom program cmds */
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *firefoxcmd[] = { "firefox", NULL };
 static const char *pcmanfmcmd[] = { "pcmanfm", NULL };
-static const char *lockscreencmd[] = { "betterlockscreen", "-l", NULL };
 
-#include "movestack.c"
+/* custom system control cmds */
+static const char *lockscreencmd[] = { "betterlockscreen", "-l", NULL };
+static const char *mutecmd[] = { "pactl", "set-sink-mute", "1", "toggle", NULL };
+static const char *volupcmd[] = { "pactl", "set-sink-volume", "1", "+5%", NULL };
+static const char *voldowncmd[] = { "pactl", "set-sink-volume", "1", "-5%", NULL };
+
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
@@ -116,6 +125,9 @@ static Key keys[] = {
 	{ SUPER,			XK_w,	   spawn,	   {.v = firefoxcmd } },
 	{ SUPER,			XK_f,	   spawn,	   {.v = pcmanfmcmd } },
 	{ SUPER,			XK_l,	   spawn,	   {.v = lockscreencmd } },
+	/*{ 0, 				XF86XK_AudioMute, spawn, {.v = mutecmd } },
+	{ 0, 				XF86XK_AudioLowerVolume, spawn, {.v = voldowncmd } },
+	{ 0, 				XF86XK_AudioRaiseVolume, spawn, {.v = volupcmd } },*/
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
