846426a27f23855037e280004f471f981c919141
sdauk555 <samdauk5@gmail.com>
2021-07-16 19:38:14 -0600

    fixed dmenu colors

--- config.h
+++ config.h
@@ -66,7 +66,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_blue, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray3, "-nf", col_gray4, "-sb", col_blue, "-sf", col_gray4, NULL };
 
 /* custom program cmds */
 static const char *termcmd[]  = { "alacritty", NULL };
