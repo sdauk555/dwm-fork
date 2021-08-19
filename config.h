258065a29fc86a133e1ca8687786e20d10834ea3
sdauk555 <samdauk5@gmail.com>
2021-08-19 17:47:51 -0600

    removed rules function

--- config.h
+++ config.h
@@ -29,15 +29,6 @@ static const char *colors[][3]      = {
 /* tagging */
 static const char *tags[] = { "⚪", "⚪", "⚪", "⚪", "⚪", "⚪", "⚪", "⚪", "⚪" };
 
-static const Rule rules[] = {
-	/* xprop(1):
-	 *	WM_CLASS(STRING) = instance, class
-	 *	WM_NAME(STRING) = title
-	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-};
-
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
