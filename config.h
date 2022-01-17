ce417ae6dbecaf6d6ab7e182e9b88d552bb89023
sdauk555 <samdauk5@gmail.com>
2022-01-17 15:56:28 -0700

    statusbutton customizations

--- config.h
+++ config.h
@@ -14,7 +14,7 @@ static const int smartgaps          = 0;        /* 1 means no outer gap when the
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char buttonbar[]	    = "X";
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "monospace:size=10", "Noto Emoji Nerd Font Complete Mono:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#141414";
 static const char col_gray2[]       = "#444444";
