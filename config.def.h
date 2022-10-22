195334730e90939545866e7a4e536fdd1a87da1c
sdauk555 <samdauk5@gmail.com>
2022-10-22 16:01:18 -0600

    updated to ver 6.4

--- config.def.h
+++ config.def.h
@@ -58,8 +58,7 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray3, "-nf", col_gray4, "-sb", col_blue, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray3, "-nf", col_gray4, "-sb", col_blue, "-sf", col_gray4, NULL };
 
 /* custom program cmds */
 static const char *termcmd[]  = { "alacritty", NULL };
@@ -88,7 +87,7 @@ static const char *setdualscreen2cmd[] = { "/home/sam/scripts/DualMonitor2.sh", 
 static const char *setsinglescreencmd[] = { "/home/sam/scripts/SingleMonitor.sh", NULL };
 static const char *setoutputcmd[] = { "/home/sam/scripts/outputSwitch.sh", NULL };
 
-static Key keys[] = {
+static cosnt Key keys[] = {
 	/* modifier                     key       			 function          argument */
 	{ SUPER,                        XK_space, 			 spawn,            {.v = dmenucmd } },
 	{ MODKEY|ControlMask,           XK_t, 	  			 spawn,            {.v = termcmd } },
@@ -152,7 +151,7 @@ static Key keys[] = {
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
-static Button buttons[] = {
+static const Button buttons[] = {
 	/* click                event mask      button          function        argument */
 	{ ClkButton,		0,		Button1,	killclient,	{0} },
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
